c8b2a149dcdda32e150b75a8d4e0f78315a722cb
sathishsuriya <sathishmd.mailme@gmail.com>
2022-01-27 10:03:47 +0530

    One Version

---
+++ ContinousIntegerationCI_Devops/src/FirstTest.java
@@ -0,0 +1,18 @@
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("selenium");
+		
+		System.out.println("java");
+		
+		System.out.println("python");
+		
+		System.out.println("DataScience");
+	}
+
+}
